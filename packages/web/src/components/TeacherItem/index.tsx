84d4ce6e5ca3075eaf8b448bf5f5c77acec4afc1
DizzySch <victorwilbert@gmail.com>
2020-08-05 17:51:00 -0300

    finalizando o front-end da aplicacao

--- packages/web/src/components/TeacherItem/index.tsx
+++ packages/web/src/components/TeacherItem/index.tsx
@@ -1,31 +1,51 @@
 import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
+import api from '../../services/api'
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://upload.wikimedia.org/wikipedia/en/6/65/Walter_White2.jpg" alt="Walter White" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Walter White</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                        <br /> <br />
-                        Um professor que descobriu que está com câncer e busca dar aulas particulares para conseguir seguir com o seu tratamento.
-                    </p>
+                 <p>{teacher.bio}</p>
             <footer>
                 <p>
                     Preço/hora
-                            <strong>R$: 20,00</strong>
+                            <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target='_blank' 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`} >
                     <img src={whatsappIcon} alt="Whatsapp" />
-                            Entrar em contato
-                        </button>
+                    Entrar em contato
+                </a>
             </footer>
         </article>
     )
